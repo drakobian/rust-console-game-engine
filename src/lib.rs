5e5b5b9fb7995c848295f05e8fd353298e68d9bb
drakobian <drake.bennion@gmail.com>
2021-08-26 21:01:58 -0600

    feat: switch to opengl (needs refactoring)

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,12 @@
 //!
 //! Better docs *definitely* coming soon 😁
 
+#[macro_use]
+extern crate glium;
+
+use glium::{glutin, Surface};
+use std::io::Cursor;
+
 use crossterm::style::{Print, StyledContent, Stylize};
 use crossterm::{cursor, execute, terminal};
 use keyboard_query::{DeviceQuery, DeviceState};
@@ -14,22 +20,25 @@ use std::io::stdout;
 pub use crossterm::style::Color;
 pub use crossterm::Result;
 
-pub struct ConsoleGameEngine<T: Rules> {
+pub struct ConsoleGameEngine {
     height: usize,
-    utils: Utils,
-    rules: T,
+    //utils: 
     width: usize,
 }
 
-impl<T> ConsoleGameEngine<T>
-where
-    T: Rules,
+#[derive(Copy, Clone, Debug)]
+pub struct Vertex {
+    position: [f32; 2],
+    color: [f32; 3],
+    //tex_coords: [f32; 2],
+}
+
+impl ConsoleGameEngine
 {
-    pub fn new(height: usize, width: usize, rules: T) -> ConsoleGameEngine<T> {
+    pub fn new(height: usize, width: usize) -> ConsoleGameEngine {
         ConsoleGameEngine {
             height,
-            utils: Utils::new(height, width),
-            rules,
+            //util
             width,
         }
     }
@@ -47,29 +56,101 @@ where
         Ok(())
     }
 
-    pub fn start(&mut self, redraw: bool) -> Result<()> {
-        self.rules.on_user_create(&mut self.utils);
+    pub fn start(self, rules: &'static mut dyn Rules) -> Result<()> {
+        
+        implement_vertex!(Vertex, position, color);
+
+        let vertex_shader_src = r#"
+            #version 140
+
+            in vec2 position;
+            in vec3 color;
+
+            out vec3 v_color;
+
+            void main() {
+                v_color = color;
+                gl_Position = vec4(position, 0.0, 1.0);
+            }
+        "#;
+
+        let fragment_shader_src = r#"
+            #version 140
+
+            in vec3 v_color;
+            out vec4 color;
+
+            void main() {
+                color = vec4(v_color, 1.0);
+            }
+        "#;
+
+        let mut event_loop = glutin::event_loop::EventLoop::new();
+        let wb = glutin::window::WindowBuilder::new().with_title("Game time!");
+        //.with_inner_size(glutin::dpi::LogicalSize::new(1024.0, 768.0));
+        let cb = glutin::ContextBuilder::new();
+        let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+        let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+        let mut utils = Utils::new(self.height, self.width);
+        rules.on_user_create(&mut utils);
 
         let mut t_p_1 = std::time::Instant::now();
-        let mut t_p_2: std::time::Instant;
-
+        
         let device_state = DeviceState::new();
 
-        loop {
-            t_p_2 = std::time::Instant::now();
+        //loop {
+        event_loop.run(move |ev, _, control_flow| {
+
+            match ev {
+                glutin::event::Event::WindowEvent { event, .. } => match event {
+                    glutin::event::WindowEvent::CloseRequested => {
+                        *control_flow = glutin::event_loop::ControlFlow::Exit;
+                        return;
+                    },
+                    _ => return,
+                },
+                glutin::event::Event::NewEvents(cause) => match cause {
+                    glutin::event::StartCause::ResumeTimeReached { .. } => {},
+                    glutin::event::StartCause::Init => (),
+                    _ => return,
+                },
+                _ => return,
+            }
+    
+            let next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16);
+            *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+            //let mut t_p_2: std::time::Instant::now();
+            let mut t_p_2 = std::time::Instant::now();
             let elapsed_time = t_p_2.duration_since(t_p_1).as_secs_f64();
             t_p_1 = t_p_2;
 
-            self.utils.keys = device_state.get_keys();
+            utils.keys = device_state.get_keys();
 
-            self.rules.on_user_update(&mut self.utils, elapsed_time);
+            rules.on_user_update(&mut utils, elapsed_time);
 
-            if redraw {
-                self.utils.redraw_screen()?
-            } else {
-                self.utils.draw_screen()?
-            };
-        }
+            let mut target = display.draw();
+            //target.clear_color(0., 0., 0.2, 1.0);
+
+            //println!("{}", &utils.index_buffer.len());
+            //println!("{}", &utils.vertex_buffer.len());
+            let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList, &utils.index_buffer).unwrap();
+            let vertex_buffer = glium::VertexBuffer::new(&display, &utils.vertex_buffer).unwrap();
+
+            target.draw(&vertex_buffer, &indices, &program, &uniform!{}, &Default::default()).unwrap();
+            
+            // now clear the index buffer and vertex_buffer on utils??
+            utils.vertex_buffer.clear();
+            utils.index_buffer.clear();
+            //println!("{}", &utils.index_buffer.len());
+            //println!("{}", &utils.vertex_buffer.len());
+            //return;
+            
+            target.finish().unwrap();
+        });
+        //}
     }
 }
 
@@ -79,6 +160,8 @@ pub struct Utils {
     pub keys: Vec<u16>,
     screen: Vec<StyledContent<char>>,
     pub width: usize,
+    pub index_buffer: Vec<u16>,
+    pub vertex_buffer: Vec<Vertex>,
 }
 
 impl Utils {
@@ -89,6 +172,8 @@ impl Utils {
             keys: vec![],
             screen: vec![' '.with(Color::Black); height * width],
             width,
+            index_buffer: vec![],
+            vertex_buffer: vec![],
         }
     }
 
@@ -123,11 +208,39 @@ impl Utils {
         }
     }
 
-    pub fn draw(&mut self, x: usize, y: usize, ch: char, color: Color) {
-        if x < self.width && y < self.height && self.screen[y * self.width + x] != ch.with(color) {
-            self.screen[y * self.width + x] = ch.with(color);
-            self.diff_coords.push((x, y));
-        }
+    pub fn draw(&mut self, x: usize, y: usize, ch: char, c_color: Color) {
+        //hmm how scale
+        let x_scale = 13. / 800.;
+        let y_scale = 15. / 600.;
+        let x_off = x as f32 * x_scale;
+        let y_off = y as f32 * y_scale;
+        let right = (x as f32 + 1.) * x_scale;
+        let down = (y as f32 + 1.) * y_scale;
+
+        let color = match c_color {
+            Color::Black => [0., 0., 0.],
+            Color::Red => [1., 0., 0.],
+            Color::Green => [0., 1., 0.],
+            Color::Blue => [0., 0., 1.],
+            Color::DarkGrey => [0.2, 0.2, 0.2],
+            Color::DarkGreen => [0., 0.4, 0.],
+            Color::DarkYellow => [0.8, 0.6, 0.2],
+            Color::DarkBlue => [0., 0.0, 0.4],
+            _ => [1., 1., 1.],
+        };
+
+        self.vertex_buffer.push(Vertex{ position: [-1. + x_off, 1. - y_off], color});
+        self.vertex_buffer.push(Vertex{ position: [-1. + right, 1. - y_off], color});
+        self.vertex_buffer.push(Vertex{ position: [-1. + x_off, 1. - down], color});
+        self.vertex_buffer.push(Vertex{ position: [-1. + right, 1. - down], color});
+    
+        let start_ind = (self.vertex_buffer.len() - 4) as u16;
+        self.index_buffer.push(start_ind);
+        self.index_buffer.push(start_ind + 1);
+        self.index_buffer.push(start_ind + 2);
+        self.index_buffer.push(start_ind + 1);
+        self.index_buffer.push(start_ind + 2);
+        self.index_buffer.push(start_ind + 3);
     }
 
     pub fn fill(&mut self, x1: usize, y1: usize, x2: usize, y2: usize, ch: char, color: Color) {
--- src/main.rs
+++ src/main.rs
@@ -158,7 +158,7 @@ impl Rules for MazeRules {
     }
 
     fn on_user_update(&mut self, utils: &mut Utils, _elapsed_time: f64) {
-        std::thread::sleep(std::time::Duration::from_millis(20));
+        //std::thread::sleep(std::time::Duration::from_millis(2));
 
         if self.visited < self.maze_width * self.maze_height {
             let neighbors = self.get_unvisited_neighbors();
@@ -174,11 +174,246 @@ impl Rules for MazeRules {
     }
 }
 
-fn main() -> Result<()> {
-    let rules = MazeRules::new(25, 40);
-    let mut game = ConsoleGameEngine::new(100, 160, rules);
-    game.construct_console()?;
-    game.start(false)?;
+struct RacerRules {
+    car_distance: f64,
+    car_position: f64,
+    current_lap_time: f64,
+    curvature: f64,
+    lap_times: Vec<f64>,
+    player_curvature: f64,
+    speed: f64,
+    track: Vec<(f64, f64)>,
+    track_curvature: f64,
+    track_distance: f64,
+}
 
-    Ok(())
+impl RacerRules {
+    fn new() -> Self {
+        RacerRules {
+            car_distance: 0.0,
+            car_position: 0.0,
+            current_lap_time: 0.0,
+            curvature: 0.0,
+            lap_times: vec![0.0; 5],
+            player_curvature: 0.0,
+            speed: 0.0,
+            track: vec![],
+            track_curvature: 0.0,
+            track_distance: 0.0,
+        }
+    }
+}
+
+impl Rules for RacerRules {
+    fn on_user_create(&mut self, _utils: &mut Utils) {
+        self.track.push((0.0, 10.0));
+        self.track.push((0.0, 250.0));
+        self.track.push((0.5, 200.0));
+        self.track.push((1.0, 200.0));
+        self.track.push((0.5, 175.0));
+        self.track.push((0.0, 200.0));
+        self.track.push((-0.5, 100.0));
+        self.track.push((-1.0, 100.0));
+        self.track.push((0.0, 200.0));
+        self.track.push((-1.0, 200.0));
+        self.track.push((1.0, 200.0));
+        self.track.push((0.0, 200.0));
+        self.track.push((0.2, 500.0));
+        self.track.push((0.0, 200.0));
+
+        for track in &self.track {
+            self.track_distance += track.1;
+        }
+    }
+
+    fn on_user_update(&mut self, utils: &mut Utils, elapsed_time: f64) {
+        if utils.keys.contains(&38) {
+            self.speed += 2.0 * elapsed_time;
+        } else {
+            self.speed -= 1.0 * elapsed_time;
+        }
+
+        let mut car_direction = 0;
+
+        if utils.keys.contains(&37) {
+            self.player_curvature -= 0.7 * elapsed_time;
+            car_direction = -1;
+        }
+
+        if utils.keys.contains(&39) {
+            self.player_curvature += 0.7 * elapsed_time;
+            car_direction = 1;
+        }
+
+        if (self.player_curvature - self.track_curvature).abs() >= 0.8 {
+            self.speed -= 2.5 * elapsed_time;
+        }
+
+        if self.speed < 0.0 {
+            self.speed = 0.0;
+        }
+        if self.speed > 1.0 {
+            self.speed = 1.0;
+        }
+
+        self.car_distance += (70.0 * self.speed) * elapsed_time;
+
+        let mut offset = 0.0;
+        let mut track_section = 0;
+
+        self.current_lap_time += elapsed_time;
+
+        if self.car_distance >= self.track_distance {
+            self.car_distance -= self.track_distance;
+            self.lap_times.insert(0, self.current_lap_time);
+            self.lap_times.pop();
+            self.current_lap_time = 0.0;
+        }
+
+        while track_section < self.track.len() && offset <= self.car_distance {
+            offset += self.track[track_section].1;
+            track_section += 1;
+        }
+
+        let target_curvature = self.track[track_section - 1].0;
+        let curvature_diff = (target_curvature - self.curvature) * elapsed_time * self.speed;
+        self.curvature += curvature_diff;
+
+        self.track_curvature += self.curvature * elapsed_time * self.speed;
+
+        // draw the sky
+        for y in 0..utils.height / 2 {
+            for x in 0..utils.width {
+                let sky_color = match y {
+                    y if y < utils.height / 4 => Color::DarkBlue,
+                    _ => Color::Blue
+                };
+                utils.draw(x, y, '█', sky_color);
+            }
+        }
+
+        // draw a hill
+        for x in 0..utils.width {
+            let hill_height = (12.0 * (x as f64 * 0.0175 + self.track_curvature).sin()).abs();
+            for y in ((utils.height as f64 / 2.0) - hill_height) as usize..(utils.height / 2) {
+                utils.draw(x, y, '█', Color::DarkYellow);
+            }
+        }
+
+        // draw road, clip, and grass
+        for y in 0..(utils.height / 2) {
+            for x in 0..utils.width {
+                let perspective = y as f64 / (utils.height as f64 / 2.0);
+                let middle = 0.5 + self.curvature * (1.0 - perspective).powf(3.0);
+                let mut road_width = 0.1 + perspective * 0.8;
+                let clip_width = road_width * 0.15;
+                road_width /= 2.0;
+
+                let left_grass = (middle - road_width - clip_width) * utils.width as f64;
+                let right_grass = (middle + road_width + clip_width) * utils.width as f64;
+                let left_clip = (middle - road_width) * utils.width as f64;
+                let right_clip = (middle + road_width) * utils.width as f64;
+
+                let y_flip = (utils.height / 2) + y;
+
+                let grass_arg: f64 = 20.0 * (1.0 - perspective).powf(3.0) + self.car_distance * 0.1;
+                let grass_color = match grass_arg.sin() {
+                    x if x > 0.0 => Color::Green,
+                    _ => Color::DarkGreen,
+                };
+
+                let clip_arg: f64 = 60.0 * (1.0 - perspective).powf(1.5) + self.car_distance;
+                let clip_color = match clip_arg.sin() {
+                    x if x > 0.0 => Color::Red,
+                    _ => Color::White,
+                };
+
+                let road_color = match track_section - 1 {
+                    0 => Color::White,
+                    _ => Color::DarkGrey,
+                } ;
+
+                match x as f64 {
+                    x if x < left_grass => utils.draw(x as usize, y_flip, '█', grass_color),
+                    x if x >= left_grass && x < left_clip => {
+                        utils.draw(x as usize, y_flip, '█', clip_color)
+                    }
+                    x if x >= left_clip && x < right_clip => {
+                        utils.draw(x as usize, y_flip, '█', road_color)
+                    }
+                    x if x >= right_clip && x < right_grass => {
+                        utils.draw(x as usize, y_flip, '█', clip_color)
+                    }
+                    x if x >= right_grass => utils.draw(x as usize, y_flip, '█', grass_color),
+                    _ => (),
+                }
+            }
+        }
+
+        // draw car
+        self.car_position = self.player_curvature - self.track_curvature;
+        let car_x =
+            utils.width as f64 / 2.0 + ((utils.width as f64 * self.car_position) / 2.0) - 6.0;
+        let car_y = (0.8 * utils.height as f64) as usize;
+
+        match car_direction {
+            1 => {
+                utils.draw_string(car_x as usize, car_y + 0, "      //####//", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 1, "         ##   ", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 2, "       ####   ", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 3, "      ####    ", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 4, "///  ####//// ", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 5, "//#######///O ", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 6, "/// #### //// ", Color::Black, true);
+            },
+            -1 => {
+                utils.draw_string(car_x as usize, car_y + 0, "\\\\####\\\\      ", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 1, "   ##         ", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 2, "   ####       ", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 3, "    ####      ", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 4, " \\\\\\\\####  \\\\\\", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 5, " O\\\\\\#######\\\\", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 6, " \\\\\\\\ #### \\\\\\", Color::Black, true);
+            },
+            _ => {
+                utils.draw_string(car_x as usize, car_y + 0, "   ||####||   ", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 1, "      ##      ", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 2, "     ####     ", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 3, "     ####     ", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 4, "|||  ####  |||", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 5, "|||########|||", Color::Black, true);
+                utils.draw_string(car_x as usize, car_y + 6, "|||  ####  |||", Color::Black, true);
+            }
+		}
+    
+        // draw stats
+        utils.draw_string(0, 1, &format!("Distance {:.2}", self.car_distance), Color::White, false);
+        utils.draw_string(0, 2, &format!("Target Curvature {:.2}", self.curvature), Color::White, false);
+        utils.draw_string(0, 3, &format!("Player Curvature {:.2}", self.player_curvature), Color::White, false);
+        utils.draw_string(0, 4, &format!("Player Speed {}", (self.speed * 70.0) as usize), Color::White, false);
+        utils.draw_string(0, 5, &format!("Track Curvature {:.2}", self.track_curvature), Color::White, false);
+    
+        let display_lap_time = |t: &f64| -> String {
+            let mins = (t / 60.0) as usize;
+            let secs = *t as usize - (mins * 60);
+            let millis = (t - secs as f64) * 10000.0;
+
+            // todo: format this betterer
+            return format!("{:02}:{:02}:{:04}", mins, secs, millis as usize);
+        };
+
+        utils.draw_string(10, 8, &display_lap_time(&self.current_lap_time), Color::White, false);
+
+        for (ind, lap_time) in self.lap_times.iter().enumerate() {
+            utils.draw_string(10, 10 + ind, &display_lap_time(lap_time), Color::White, false);
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    //let rules = MazeRules::new(25, 40);
+    let rules = RacerRules::new();
+    let game = ConsoleGameEngine::new(75, 120);
+    //game.construct_console()?;
+    game.start(Box::leak(Box::new(rules)))
 }
